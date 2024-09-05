b1254c6c3b53a632d9ef12428370fd59060b3d8f
IbanezAhista <ibanezcentivoliaahista@mail.ugm.ac.id>
2024-09-05 17:31:00 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Selamat Datang di Halaman Home</h2>
+    <p>Ini adalah contoh web sederhana dengan controller pada Laravel.</p>
+</body>
+</html>
